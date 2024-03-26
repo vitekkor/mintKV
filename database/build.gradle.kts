1ee6d28d04aef82ff620c40dcdb7f124d361b558
vitekkor <viktor.korotkikh@gmail.com>
2024-03-26 22:09:14 +0300

    #1: add Main functon

--- database/build.gradle.kts
+++ database/build.gradle.kts
@@ -3,5 +3,7 @@ plugins {
 }
 
 dependencies {
-    //
+    // Logging
+    implementation("org.slf4j:slf4j-api:2.0.12")
+    implementation("ch.qos.logback:logback-classic:1.5.3")
 }
---
+++ database/src/main/java/com/mint/MintKVDatabase.java
@@ -0,0 +1,13 @@
+package com.mint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MintKVDatabase {
+    private static final Logger log = LoggerFactory.getLogger(MintKVDatabase.class);
+
+    public static void main(String[] args) {
+        log.error("MintKVDatabase not implemented");
+        System.exit(1);
+    }
+}
