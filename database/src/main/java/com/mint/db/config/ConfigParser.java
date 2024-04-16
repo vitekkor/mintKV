e7afb24a06a4b7e4c8fbf5aaf1e77e90d0ed0784
axothy <globaltouareg@gmail.com>
2024-04-16 13:33:50 +0300

    fix codestyle

--- database/src/main/java/com/mint/db/config/ConfigParser.java
+++ database/src/main/java/com/mint/db/config/ConfigParser.java
@@ -1,6 +1,9 @@
 package com.mint.db.config;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
