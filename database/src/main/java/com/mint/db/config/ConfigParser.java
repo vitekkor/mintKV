abe4298d6cf43cec3bb4ac5dd5ace3e059492e18
axothy <globaltouareg@gmail.com>
2024-04-16 13:30:47 +0300

    parse fix

--- database/src/main/java/com/mint/db/config/ConfigParser.java
+++ database/src/main/java/com/mint/db/config/ConfigParser.java
@@ -1,15 +1,12 @@
 package com.mint.db.config;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public final class ConfigParser {
-    private static final String NODE_CONFIG_PATH_DEFAULT = "database/src/main/resources/node-config.yaml";
+    private static final String NODE_CONFIG_PATH_DEFAULT = "node-config.yaml";
     private static final String NODE_CONFIG_ENV_PROPERTY = "mint.config.location";
 
     private ConfigParser() {
@@ -19,13 +16,17 @@ public final class ConfigParser {
     public static NodeConfig parseConfig() throws IOException {
         String configPath = System.getProperty(NODE_CONFIG_ENV_PROPERTY);
 
-        File file = new File(Objects.requireNonNullElse(configPath, NODE_CONFIG_PATH_DEFAULT));
+        InputStream inputStream = ConfigParser.class
+                .getClassLoader()
+                .getResourceAsStream(
+                        Objects.requireNonNullElse(configPath, NODE_CONFIG_PATH_DEFAULT)
+                );
 
         int port = 0;
         int nodeId = 0;
         List<String> cluster = new ArrayList<>();
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 line = line.trim();
