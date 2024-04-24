00e4da81dffe27c70a94cd6a14f4648adde36eba
axothy <globaltouareg@gmail.com>
2024-04-25 00:27:53 +0300

    periodic task execution logic

---
+++ database/src/main/java/com/mint/db/impl/RaftActor.java
@@ -0,0 +1,51 @@
+package com.mint.db.impl;
+
+import io.grpc.Status;
+import io.grpc.StatusException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class RaftActor {
+    private static final Logger log = LoggerFactory.getLogger(RaftActor.class);
+    private static final Random rand = new Random();
+    private static final int POOL_SIZE = 1;
+    private final ScheduledExecutorService scheduledExecutor;
+    private final Runnable heartBeatTask;
+    private ScheduledFuture<?> scheduledFuture;
+    private static final long HEARTBEAT_DELAY_MS = 5000L;
+    private boolean amILeader = false; // ?
+
+    public RaftActor() {
+        this.scheduledExecutor = Executors.newScheduledThreadPool(POOL_SIZE);
+        this.heartBeatTask = () -> System.out.println("STUB");
+    }
+
+    private void sleepBeforeElections() {
+        if (scheduledFuture != null) {
+            scheduledFuture.cancel(false);
+        }
+
+        scheduledFuture = scheduledExecutor.scheduleAtFixedRate(
+                heartBeatTask,
+                HEARTBEAT_DELAY_MS,
+                HEARTBEAT_DELAY_MS + rand.nextLong(1000L, 5000L),
+                TimeUnit.MILLISECONDS);
+
+    }
+
+    public void onHeartBeat() throws StatusException {
+        if (amILeader) {
+            throw new StatusException(Status.FAILED_PRECONDITION);
+        }
+
+        log.info("heartbeat");
+        sleepBeforeElections();
+    }
+
+}
