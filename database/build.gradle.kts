2f417de410171fdadae5a39d0b22ce956b5673ca
goshadalbeev <55684784+goshadalbeev@users.noreply.github.com>
2024-05-07 20:43:15 +0300

    add some groundwork

--- database/build.gradle.kts
+++ database/build.gradle.kts
@@ -1,5 +1,12 @@
 plugins {
-    `java-configuration`
+    id("java")
+    id("com.google.protobuf") version "0.9.4"
+    id("idea")
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
 }
 
 dependencies {
@@ -7,7 +14,40 @@ dependencies {
     implementation("org.slf4j:slf4j-api:2.0.12")
     implementation("ch.qos.logback:logback-classic:1.5.3")
     implementation("org.yaml:snakeyaml:2.2")
-    implementation("io.grpc:grpc-all:1.63.0")
+    implementation ("io.grpc:grpc-protobuf:1.63.0")
+    implementation ("io.grpc:grpc-services:1.63.0")
+    implementation ("io.grpc:grpc-stub:1.63.0")
+//    implementation("io.grpc:grpc-all:1.63.0")
+    compileOnly("org.apache.tomcat:annotations-api:6.0.53")
 
-
+    implementation("com.google.protobuf:protobuf-java:3.6.1")
 }
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.6.1"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.63.0"
+        }
+    }
+    generateProtoTasks {
+        ofSourceSet("main").forEach {
+            it.plugins {
+                id("grpc") { }
+            }
+        }
+    }
+}
+
+sourceSets {
+    main {
+        java {
+            srcDirs("build/generated/source/proto/main/grpc")
+            srcDirs("build/generated/source/proto/main/java")
+        }
+    }
+}
+
+
