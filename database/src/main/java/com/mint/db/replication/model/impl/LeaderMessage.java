f31260d8743ffbb4594fcb9bf265323b1af69ec8
Nikita Prokopyev <nikpro200125@gmail.com>
2024-05-10 15:02:10 +0300

    fix codestyle

--- database/src/main/java/com/mint/db/replication/model/impl/LeaderMessage.java
+++ database/src/main/java/com/mint/db/replication/model/impl/LeaderMessage.java
@@ -4,11 +4,11 @@ import com.mint.db.Raft;
 import com.mint.db.replication.model.Message;
 
 public class LeaderMessage extends Message {
-    private final String LeaderUrl;
+    private final String leaderUrl;
     private final Raft.AppendEntriesRequest request;
 
     public LeaderMessage(String leaderUrl, Raft.AppendEntriesRequest request) {
-        this.LeaderUrl = leaderUrl;
+        this.leaderUrl = leaderUrl;
         this.request = request;
     }
 }
