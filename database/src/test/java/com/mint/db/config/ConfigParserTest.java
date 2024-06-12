4fc19fd7ae3e5a7b413ba73d5ce2b12b7bbc313f
atveritin <tveritin09@gmail.com>
2024-06-12 20:44:53 +0300

    #59

--- database/src/test/java/com/mint/db/config/ConfigParserTest.java
+++ database/src/test/java/com/mint/db/config/ConfigParserTest.java
@@ -13,7 +13,7 @@ class ConfigParserTest {
 
     @Test
     void testParseConfig_DefaultPath() throws FileNotFoundException {
-        NodeConfig expectedConfig = new NodeConfig(8080, 1, "logs/", 200L, Arrays.asList("node1", "node2"));
+        NodeConfig expectedConfig = new NodeConfig(9090, 8080, 1, "logs/", 200L, Arrays.asList("node1", "node2"));
 
         NodeConfig actualConfig = ConfigParser.parseConfig();
 
@@ -30,6 +30,7 @@ class ConfigParserTest {
 
         try {
             NodeConfig expectedConfig = new NodeConfig(
+                    9090,
                     8800,
                     11,
                     "/var/logs2",
